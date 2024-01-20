f64d73420d785d98236ee1dbca530eb4a5fdabb1
Nicolai Parlog <nicolai@nipafx.dev>
2024-01-20 23:50:35 +0100

    Introduce store queries [WIP]

--- ginevra/src/main/java/dev/nipafx/ginevra/execution/MapStore.java
+++ ginevra/src/main/java/dev/nipafx/ginevra/execution/MapStore.java
@@ -5,8 +5,10 @@ import dev.nipafx.ginevra.outline.Store;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static java.util.stream.Collectors.joining;
 
@@ -14,15 +16,18 @@ public class MapStore implements Store {
 
 	private final Map<DocCollection, List<Document<?>>> documents;
 	private final List<Document<?>> rootDocuments;
+	private final Set<Query<?>> queries;
 
 	public MapStore() {
 		documents = new HashMap<>();
 		rootDocuments = new ArrayList<>();
+		queries = new HashSet<>();
 	}
 
 	@Override
 	public void store(Document<?> doc) {
 		rootDocuments.add(doc);
+		// TODO: detect changes and mark impacted queries as dirty
 	}
 
 	@Override
@@ -33,8 +38,23 @@ public class MapStore implements Store {
 	}
 
 	@Override
-	public void commit() {
+	public List<Query<?>> commit() {
+		// TODO: return dirty queries
+		return List.of();
+	}
 
+	@Override
+	public <RESULT extends Record> RESULT query(RootQuery<RESULT> query) {
+		queries.add(query);
+		// TODO: evaluate query
+		return null;
+	}
+
+	@Override
+	public <RESULT extends Record> List<RESULT> query(CollectionQuery<RESULT> query) {
+		queries.add(query);
+		// TODO: evaluate query
+		return null;
 	}
 
 	@Override
--- ginevra/src/main/java/dev/nipafx/ginevra/outline/Store.java
+++ ginevra/src/main/java/dev/nipafx/ginevra/outline/Store.java
@@ -2,6 +2,8 @@ package dev.nipafx.ginevra.outline;
 
 import dev.nipafx.ginevra.outline.Document.Data;
 
+import java.util.List;
+
 /**
  * Stores all {@link Document}s produced by various {@link Source}s and {@link Transformer}s.
  */
@@ -11,10 +13,22 @@ public interface Store {
 
 	void store(DocCollection collection, Document<?> doc);
 
-	void commit();
+	List<Query<?>> commit();
+
+	<RESULT extends Record> RESULT query(RootQuery<RESULT> query);
+
+	<RESULT extends Record> List<RESULT> query(CollectionQuery<RESULT> query);
 
 	record None() implements Data { }
 
 	record DocCollection(String name) { }
+
+	sealed interface Query<RESULT extends Record> { }
+
+	record RootQuery<RESULT extends Record>(Class<RESULT> resultType)
+			implements Query<RESULT> { }
+
+	record CollectionQuery<RESULT extends Record>(String collection, Class<RESULT> resultType)
+			implements Query<RESULT> { }
 
 }
